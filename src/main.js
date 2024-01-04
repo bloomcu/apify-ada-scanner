8ed92127ebbe76b6fe7f8ed840445a062e7f7a50
Ryan Harmon <ryan@bloomcu.com>
2024-01-03 23:34:45 -0700

    Implement FAE

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Actor } from 'apify';
-import { PuppeteerCrawler } from 'crawlee';
+import { PuppeteerCrawler, Dataset } from 'crawlee';
 
 await Actor.init();
 const input = await Actor.getInput(); // The parameters you passed to the actor
@@ -8,11 +8,23 @@ const crawler = new PuppeteerCrawler({
 
   async requestHandler({ request, page, enqueueLinks, log }) {
       const title = await page.title();
-      
+
+      // Inject and execute FAE
+      await page.addScriptTag({path: './vendor/main.js'});
+      const results = await page.evaluate(()=>executeTest());
+
+      // Store the results
+      await Dataset.pushData({
+          title: title,
+          results: results,
+      })
+
       // Log anything that might be useful to see during crawling job.
       log.info(`Checking '${title}' at url: ${request.loadedUrl}`);
+      log.info(`Found '${JSON.parse(results).rule_results.length}' violations`);
 
-      await enqueueLinks();
+      // Enqueue discovered links
+      // await enqueueLinks();
   },
 
   maxRequestsPerCrawl: 200,
